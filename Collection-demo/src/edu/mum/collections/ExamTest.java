e4ecc63e12a0b55940eb07bb417f9a25d0bba7e6
AmanuelGebru <AmanuelGebru@192.168.0.12>
2017-07-06 21:46:21 -0400

    fourth Commit

--- Collection-demo/src/edu/mum/collections/ExamTest.java
+++ Collection-demo/src/edu/mum/collections/ExamTest.java
@@ -1,6 +1,8 @@
 package edu.mum.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ExamTest {
@@ -19,13 +21,36 @@ public class ExamTest {
             persons.add(new Person(1,"Berhe",25));           
             persons.add(new Person(2,"Hagos",30));
             persons.add( new Person(3,"Kidane",23));
-            persons.add(new Person(4,"Mebre",40));           
+            persons.add(new Person(4,"Mebre",40));  
+            persons.add(new Person(6,"Mohamed",29));
             print(persons);
             
             AddPerson(new Person(5,"Belay",42));
             System.out.println("\nAfter Adding new Person ...\n");
             print(persons);
+            
+        System.out.println("\n Sorting the Collections using their ages ....\n");
+        System.out.println("===================================================\n");
+        Collections.sort(persons,(p1,p2)->p1.getAge()-p2.getAge()); // using Comparator Java 8
+          print(persons);
+          System.out.println("===================================================\n");
+          
+          System.out.println("\n Comparing persons by First Name...\n");
+          System.out.println("===================================================\n");
+          Collections.sort(persons, compBy_name);
+          print(persons);
+          System.out.println("===================================================\n");
 	}
+	
+	private static Comparator<Person> compBy_name=new Comparator<Person>(){
+
+		@Override
+		public int compare(Person per1, Person per2) {
+			
+			return per1.getFirstName().compareTo(per2.getFirstName());// Ascending Order
+		}
+		
+	};
 
 	private static void print(List<Person> persons) {
 		persons.stream().forEach(System.out::println);
--- Collection-demo/src/edu/mum/collections/Person.java
+++ Collection-demo/src/edu/mum/collections/Person.java
@@ -11,6 +11,37 @@ public class Person {
 		this.age = age;
 	}
 
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", firstName=" + firstName + ", age=" + age + "]";
